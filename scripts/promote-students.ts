5d3df0a28dbfd85799f201233636e14b6dca0cb2
nafisreza <bigganinafis@gmail.com>
2025-07-15 04:30:31 +0600

    script: promote semester on year end

---
+++ scripts/promote-students.ts
@@ -0,0 +1,8 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// await prisma.student.updateMany({
+//   where: { currentSemester: { lt: 8 } }, // Only promote if not graduated
+//   data: { currentSemester: { increment: 1 } }
+// });
