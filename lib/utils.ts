cc0fd9460f4599bec410f6973732387493afa3d1
nafisreza <bigganinafis@gmail.com>
2025-08-23 09:11:12 +0600

    chore: modularize password generation

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+export function generatePassword(length = 12): string {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
